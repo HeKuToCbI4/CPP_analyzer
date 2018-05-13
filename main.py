b596e60213db320c2a72ae05d4bc8ac0430f866b
Nikita <leonov2424@mail.ru>
2018-05-13 14:11:56 +0400

    Merged other changes

---
+++ main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+from UI import UI
+from src.buffer_overflow import BufOverflowParser
+from src.format_string import FormatStringParser
+from src.sql_injection import SQLInjectionParser
+from src.crypto_gen import RandomGenParser
+from src.exec_of_commands import ExecOfCommands
+
+HANDLER = {
+    "Buffer Overflow": BufOverflowParser,
+    "Format String Vulnerability": FormatStringParser,
+    "SQL injection": SQLInjectionParser,
+    "Command Injection": ExecOfCommands,
+    # "Neglect of Error Handling":		None,
+    # "Bad Data Storage Management":		None,
+    # "Data Leak":						None,
+    "Not Crypto-resistant Algorithms": RandomGenParser,
+    # "Integer Overflow":				None,
+    # "Race Condition":					None,
+    # "Readers–writers problem":			None
+}
+
+
+def commentRemover(text):
+    import re
+    pattern = re.compile(r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"', re.DOTALL | re.MULTILINE)
+    replacer = lambda match: None if match.group(0).startswith('/') else match.group(0)
+    return re.sub(pattern, replacer, text)
+
+
+from re import match
+
+CLEAN_CODE = lambda program: [line.lstrip() for line in commentRemover(open(program, 'r').read()).splitlines() if
+                              not match(r'^\s*$', line)]
+
+ui = UI(HANDLER.keys())  #
+ui.StartMain(lambda vulnerability, program: HANDLER[vulnerability]().parse(CLEAN_CODE(program)))
